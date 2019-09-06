7b5535b4d1acd31ea7d7afca05b365c1d4491446
Erika Miwa <erikamiwa612@gmail.com>
2019-09-06 11:44:04 -0700

    add comand

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use('/api/exercises', require('./routes/api/exercises'));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('/frontend/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname), 'frontend', 'build', 'index.html')
+    res.sendFile(path.join(__dirname), 'frontend', 'build', )
   })
 }
 
