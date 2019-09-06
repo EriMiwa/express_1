ee2270afd8294cc4088eea495e709b79c618516d
Erika Miwa <erikamiwa612@gmail.com>
2019-09-06 15:00:51 -0700

    change index.js

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ app.use('/api/exercises', require('./routes/api/exercises'));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join('frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname), 'frontend', 'build', )
-  })
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')); // relative path
+  });
+
 }
 
 
